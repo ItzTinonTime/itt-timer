b95da4b03cc370ff9e5f8ee13bf9f39b5bb5f997
FLORIAN REINERTZ <z.zentrox@gmail.com>
2026-03-02 19:10:50 +0100

    Add German and French language support for timer messages

--- lua/timer/lang/lang_de.lua
+++ lua/timer/lang/lang_de.lua
@@ -13,4 +13,10 @@
 
 Timer.Language["de"] = {
     ["frame_title"] = "Timer",
+    ["submit"] = "Bestätigen",
+    ["cancel"] = "Abbrechen",
+    ["hour"] = "Stunden",
+    ["minute"] = "Minuten",
+    ["second"] = "Sekunden",
+    ["invalid_duration"] = "Bitte eine Dauer größer als 0 einstellen.",
 }
--- lua/timer/lang/lang_fr.lua
+++ lua/timer/lang/lang_fr.lua
@@ -13,4 +13,10 @@
 
 Timer.Language["fr"] = {
     ["frame_title"] = "Timer",
+    ["submit"] = "Confirmer",
+    ["cancel"] = "Annuler",
+    ["hour"] = "Heures",
+    ["minute"] = "Minutes",
+    ["second"] = "Secondes",
+    ["invalid_duration"] = "Veuillez définir une durée supérieure à 0.",
 }
